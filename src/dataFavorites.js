4caa3215ce0a10616622422ab580050f88edcd90
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-07-10 20:12:36 -0300

    Foi feito dentro do tableFavorites as funções createRow, e atualizei a função update

--- src/dataFavorites.js
+++ src/dataFavorites.js
@@ -1,5 +1,27 @@
 export class dataFavorites{
     constructor(root){
         this.root = document.querySelector(root)
+        this.load()
+    }
+    load(){
+        this.entries = [
+            {
+                name:"Mayk Brito",
+                login:"maykbrito",
+                public_repos: 120,
+                followers: 7000
+            },
+            {
+                name:"Diego Fernandes",
+                login:"diego3g",
+                public_repos: 120,
+                followers: 7000
+            }
+        ]
+    }
+    delete(user){
+        const verification = this.entries.filter(entry => entry.login !== user.login)
+        this.entries = verification
+        this.update()
     }
 }
--- src/tableFavorites.js
+++ src/tableFavorites.js
@@ -1,18 +1,59 @@
 import { dataFavorites } from "./dataFavorites.js"
 
-export class tableFavorites extends dataFavorites{
-    constructor(root){
-        super(root)
-        
-        this.tbody = this.root.querySelector("tbody")
-        this.update()
-    }
-    update(){
-        this.removeAllTr()
-    }
-    removeAllTr(){
-        this.tbody.querySelectorAll('tr').forEach(tr=>{
-            tr.remove()
-        })
-    }
+
+export class tableFavorites extends dataFavorites {
+  constructor(root) {
+    super(root)
+
+    this.tbody = this.root.querySelector("tbody")
+    this.update()
+  }
+  update() {
+    this.removeAllTr()
+    this.entries.forEach(data=>{
+        const row = this.createRow()
+        row.querySelector('td.user img').src = `https://github.com/${data.login}.png`
+        row.querySelector('td.user img').alt = `Imagem de ${data.name}`
+        row.querySelector('td.user a').href = `https://github.com/${data.login}`
+        row.querySelector('td.user p').textContent = `${data.name}`
+        row.querySelector('td.user span').textContent = `${data.login}`
+        row.querySelector('td.repositories').textContent = `${data.public_repos}`
+        row.querySelector('td.followers').textContent = `${data.public_repos}`
+
+        row.querySelector('.action').onclick = ()=>{
+            const isOk = confirm("Deseja mesmo excluir esse favorito")
+            if(isOk){
+                this.delete(data)
+            }
+        }
+
+        this.tbody.append(row)
+    })
+    return
+  }
+  createRow() {
+    const tr = document.createElement("tr")
+
+    tr.innerHTML = `                        
+    <td class="user">
+        <img src="https://github.com/maykbrito.png" alt="Imagem de Mayk Brito">
+        <a href="https://github.com/maykbrito" target="_blank">
+            <p>Mayk Brito</p>
+            <span>/maykbrito</span>
+        </a>
+    </td>
+    <td class="repositories">1000</td>
+    <td class="followers">100</td>
+    <td class="action">
+        <button>Remover</button>
+    </td>
+    `
+
+    return tr
+  }
+  removeAllTr() {
+    this.tbody.querySelectorAll("tr").forEach((tr) => {
+      tr.remove()
+    })
+  }
 }
